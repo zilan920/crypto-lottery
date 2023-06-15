43c1d706b0467ad71ddefb8700008ba81ad5be14
zihan.fang <zihan.fang@bitdeer.com>
2023-06-15 18:32:59 +0800

    page

--- src/components/crypto-lottery.tsx
+++ src/components/crypto-lottery.tsx
@@ -12,12 +12,9 @@ const CryptoLottery: React.FC = () => {
     const [selectedNum, setSelectedNum] = useState<number>(0)
     const [address, setAddress] = useState<string>('');
     const [key, setKey] = useState<string>('')
-    const [drawing, setDrawing] = useState<boolean>(false)
 
     const drawOne = (index: number, cube: string) => {
-        if (!drawing) {
-            return;
-        }
+
         if (selected[index] === undefined) {
             selected[index] = cube
             setSelectedNum(n => {
@@ -30,12 +27,8 @@ const CryptoLottery: React.FC = () => {
         console.log(selected)
     }
 
-    const start = () => {
-        setDrawing(true)
-    }
 
     const handleClearClick = () => {
-        setDrawing(false)
         setSelected([]);
         setAddress('');
         setKey('')
@@ -50,14 +43,15 @@ const CryptoLottery: React.FC = () => {
         setAddress(address);
     }
 
+    const openOnEtherScan = () => {
+        window.open(`https://etherscan.io/address/${address}`)
+    }
+
     return (
         <div className="crypto-lotter-container">
-            <button className="start-button" onClick={start}>
-                Start
-            </button>
             <button className="clear-button" onClick={handleClearClick}>Clear</button>
-            {key && <div className="key">Private Key: {key}</div>}
-            {address && <div className="address">Generated Ethereum Address: {address}</div>}
+            {<div className="key">Private Key: 0x{key}</div>}
+            {<div className="address" onClick={openOnEtherScan}>Address(click to open): {address}</div>}
             <div className="grid">
                 {grid.map((row, rowIndex) => (
                     <div key={`row-${rowIndex}`} className={`row`}>
